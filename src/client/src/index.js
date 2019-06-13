71dd203313a09db9c795550a1f7bff6e2ae64610
Ruslan Karachun <free.changer@gmail.com>
2019-06-13 10:23:27 +0300

    front: added crypto library

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as crypto from 'crypto-browserify';
+
+console.log(typeof crypto.publicEncrypt)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
